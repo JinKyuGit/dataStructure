99c324603f0cf57cdbfaa1f317fa14409225172f
jinkyu <jinkyu@218.38.137.27>
2023-03-12 14:13:27 +0900

    비재귀적 방법 추가.

--- src/datastructure_java/datastructure/etc/Euclidean.java
+++ src/datastructure_java/datastructure/etc/Euclidean.java
@@ -20,6 +20,8 @@ public class Euclidean {
 		//최대공약수는 두 수의 곱 / 최대공약수이다.
 		System.out.println("최소공배수 : "+copy[0]*copy[1]/e.getDivisor(result[0], result[1]));
 		
+		//System.out.println("최대공약수 : "+e.gdc(num[0], num[1]));
+		
 	}
 	
 	public int getDivisor(int num1, int num2) {
@@ -91,6 +93,21 @@ public class Euclidean {
 	}
 	
 	
-	
+	//비재귀 방법.
+	public int gdc(int a, int b) {
+		
+		//a가 큰 수
+		
+		while(b != 0) {
+			
+			int r = a % b;
+			
+			a = b;
+			b = r;
+			
+		}		
+		return a;
+
+	}
 
 }
